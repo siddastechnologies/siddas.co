51922be243badd264c419d6121790efb16fac630
Siddas <siddas.arun@gmail.com>
2025-07-05 16:19:44 +0000

    primary color Teal #008080 background color light gry #E0e0e0 Accent Col

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark h-full">
+    <html lang="en" className="h-full">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
